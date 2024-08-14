00067bac51d052e963cceb665cc2e4a0a633c9e9
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2024-08-14 14:10:21 -0700

    fix build

--- src/swap/index.ts
+++ src/swap/index.ts
@@ -4,7 +4,6 @@ export { SwapAmountInput } from './components/SwapAmountInput';
 export { SwapButton } from './components/SwapButton';
 export { SwapMessage } from './components/SwapMessage';
 export { SwapSettings } from './components/SwapSettings';
-export { SwapSettingsContainer } from './components/SwapSettingsContainer';
 export { SwapToggleButton } from './components/SwapToggleButton';
 export { buildSwapTransaction } from './utils/buildSwapTransaction';
 export { getSwapQuote } from './utils/getSwapQuote';
